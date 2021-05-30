42cb94448f6b07880c9c364cf0348f3f1f0b1cdc
Alvaro Valladares <alvaros.valladaresc@konradlorenz.edu.co>
2021-05-29 21:29:06 -0500

    Cambios hechos hoy 29 de mayo del 2021, muchas gracias. Parte 7

---
+++ src/main/java/konrad/edu/co/kdd/repository/AsuntoRepository.java
@@ -0,0 +1,12 @@
+package konrad.edu.co.kdd.repository;
+
+import konrad.edu.co.kdd.entity.Asunto;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Alvaro
+ */
+public interface AsuntoRepository extends CrudRepository<Asunto, Integer> {
+
+}
